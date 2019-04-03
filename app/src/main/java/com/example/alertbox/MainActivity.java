e606d87787a01db840952b5349ba87b08ee43807
sooraj <soorajsuneesh@gmail.com>
2019-04-02 20:19:19 -0400

    new

--- app/src/main/java/com/example/alertbox/MainActivity.java
+++ app/src/main/java/com/example/alertbox/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.alertbox;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,4 +24,65 @@ public class MainActivity extends AppCompatActivity {
         return true;
        // return super.onCreateOptionsMenu(menu);
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String msg = "";
+        switch (item.getItemId()){
+            case R.id.menuFirst:
+                msg = "menuFirst";
+                showDialog();
+                break;
+            case R.id.menuSecond:
+                msg = "second";
+                break;
+            case R.id.menuThird:
+                msg = "second";
+                break;
+            case R.id.menuLogout:
+                msg = "second";
+                break;
+        }
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+    private void showDialog()
+    {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        // set title
+        alertDialogBuilder.setTitle("Your Title");
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage("Click yes to exit!")
+                .setCancelable(false)
+                .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, close
+                        // current activity
+                        MainActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+                        dialog.cancel();
+                    }
+                });
+
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+
+    }
+
+
+
+
+
 }
